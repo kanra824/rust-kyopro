ac7420dffc6854c3615cb1c68708945cbe411fcc
Yohei Maeda <mdyh2a380824@gmail.com>
2023-07-29 05:28:57 +0900

    Abstract Segment Tree (#9)

--- src/library/structure/segment_tree.rs
+++ src/library/structure/segment_tree.rs
@@ -1,51 +1,68 @@
-
-pub struct SegmentTree {
+pub struct SegmentTree<T, F, G>
+where 
+    T: Clone + Copy,
+    F: Fn(T, T) -> T,
+    G: Fn(T, T) -> T
+{ 
     n: usize,
-    v: Vec<i64>,
+    v: Vec<T>,
+    f: F,
+    g: G,
+    zero: T,
 }
 
-impl SegmentTree {
-    pub fn new(n: usize, v: Vec<i64>) -> Self {
+impl<T, F, G> SegmentTree<T, F, G>
+where 
+    T: Clone + Copy,
+    F: Fn(T, T) -> T,
+    G: Fn(T, T) -> T
+{
+    pub fn new(n: usize, v: Vec<T>, f: F, g: G, zero: T) -> Self {
         let mut n_ = 1;
         while n_ < n {
             n_ *= 2;
         }
 
-        let mut v_ = vec![0; 2 * n_];
+        let mut v_ = vec![zero; 2 * n_];
         for i in 0..n {
             v_[i+1] = v[i];
         }
         for i in (0..=n_-1).rev() {
-            v_[i] = v_[i*2] + v_[i*2+1];
+            v_[i] = f(v_[i*2], v_[i*2+1]);
         }
 
         SegmentTree {
             n: n_,
             v: v_,
+            f,
+            g,
+            zero
         }
     }
 
-    pub fn update(&mut self, i: usize, x: i64) {
-        self.v[self.n+i] += x;
+    pub fn update(&mut self, i: usize, x: T) {
+        self.v[self.n+i] = (self.g)(self.v[self.n+i], x);
         let mut now = (self.n + i) / 2;
         while now > 0 {
-            self.v[now] = self.v[now*2] + self.v[now*2+1];
+            self.v[now] = (self.f)(self.v[now*2], self.v[now*2+1]);
             now /= 2;
         }
     }
 
-    fn query_(&self, l: usize, r: usize, k: usize, a: usize, b: usize) -> i64 {
+    fn query_(&self, l: usize, r: usize, k: usize, a: usize, b: usize) -> T {
         if r <= a || b <= l {
-            return 0;
+            return self.zero;
         }
         if a <= l && r <= b {
             return self.v[k];
         }
 
-        self.query_(l, (l+r)/2, 2*k, a, b) + self.query_((l+r)/2, r, 2*k+1, a, b)
+        let val1 = self.query_(l, (l+r)/2, 2*k, a, b);
+        let val2 = self.query_((l+r)/2, r, 2*k+1, a, b);
+        (self.f)(val1, val2)
     }
 
-    pub fn query(&self, a: usize, b: usize) -> i64 {
+    pub fn query(&self, a: usize, b: usize) -> T {
         self.query_(0, self.n, 1, a, b)
     }
 
@@ -71,7 +88,7 @@ fn main() {
     let q = v_in[1];
 
     let v = vec![0; n];
-    let mut st = SegmentTree::new(n, v);
+    let mut st = SegmentTree::new(n, v, |a, b| a + b, |a, b| a + b, 0);
 
     for _ in 0..q {
         let v_in: Vec<i64> = input_vec();
