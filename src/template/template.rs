828a1093c37d3dd112f3af891df5798db26f2fc1
kanra824 <kanra824@DESKTOP-SRSU4FU.localdomain>
2023-07-29 02:05:18 +0900

    add allow

--- src/template/template.rs
+++ src/template/template.rs
@@ -1,7 +1,10 @@
 use proconio::input;
+#[allow(unused_imports)]
 use proconio::marker::{Usize1, Isize1, Chars};
 
+#[allow(unused)]
 const MOD998: i64 = 998244353;
+#[allow(unused)]
 const MOD107: i64 = 1000000007;
 
 fn main() {
