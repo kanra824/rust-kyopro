d52f0cfbd25d8e3c8f357f30908f30c688cea932
kanra824 <kanra824@DESKTOP-SRSU4FU.localdomain>
2024-05-15 22:17:04 +0900

    Implement Trie

--- src/library/structure.rs
+++ src/library/structure.rs
@@ -1,3 +1,4 @@
 pub mod lazy_segment_tree;
 pub mod segment_tree;
+pub mod trie;
 pub mod union_find;
---
+++ src/library/structure/trie.rs
@@ -0,0 +1,58 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Trie {
+    cnt: i64,
+    end: i64,
+    child: Vec<Option<Box<Trie>>>,
+}
+
+impl Trie {
+    pub fn new() -> Self {
+        Trie {
+            cnt: 0,
+            end: 0,
+            child: vec![None; 26],
+        }
+    }
+
+    pub fn new_child() -> Option<Box<Self>> {
+        Some(Box::new(Trie {
+            cnt: 0,
+            end: 0,
+            child: vec![None; 26],
+        }))
+    }
+
+    pub fn add(&mut self, s: &Vec<char>) {
+        let mut now = self;
+        now.cnt += 1;
+        for i in 0..s.len() {
+            let idx = s[i] as usize - 'a' as usize;
+            if now.child[idx].is_none() {
+                now.child[idx] = Self::new_child();
+            }
+            now = now.child[idx].as_mut().unwrap().as_mut();
+            now.cnt += 1;
+        }
+        now.end += 1;
+    }
+
+    pub fn calc_lcp_sum(&mut self, s: &Vec<char>) -> i64 {
+        let mut now = self;
+        let mut res = 0;
+        let mut depth = 0;
+        let mut prevcnt = now.cnt;
+        for i in 0..s.len() {
+            let idx = s[i] as usize - 'a' as usize;
+            if now.child[idx].is_none() {
+                res += now.cnt * depth;
+                return res;
+            }
+            now = now.child[idx].as_mut().unwrap().as_mut();
+            res += (prevcnt - now.cnt) * depth;
+            depth += 1;
+            prevcnt = now.cnt;
+        }
+        res += now.cnt * depth;
+        res
+    }
+}
